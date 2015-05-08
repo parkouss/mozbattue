a80bd88e399fb46953dd78f521b373d163221e12
Julien Pagès <j.parkouss@gmail.com>
2015-05-08 10:03:31 +0200

    avoid unnecessary requests when triggering the build for the revision we got

--- mozbattue/trigger.py
+++ mozbattue/trigger.py
@@ -20,22 +20,25 @@ def trigger_jobs(buildername, revision, back_revisions=30, times=30,
                  dry_run=False):
     buildername = sanitize_buildername(buildername)
     repo_url = query_repo_url_from_buildername(buildername)
-    push_info = query_revision_info(repo_url, revision)
     repo_name = query_repo_name_from_buildername(buildername)
-    end_id = int(push_info["pushid"])  # newest revision
-    start_id = end_id - back_revisions
-    revlist = query_pushid_range(repo_url=repo_url,
-                                 start_id=start_id,
-                                 end_id=end_id)
-    rev = revlist[-1]
+
+    if back_revisions >= 0:
+        # find the revision *back_revisions* before the one we got
+        push_info = query_revision_info(repo_url, revision)
+        end_id = int(push_info["pushid"])  # newest revision
+        start_id = end_id - back_revisions
+        revlist = query_pushid_range(repo_url=repo_url,
+                                     start_id=start_id,
+                                     end_id=end_id)
+        revision = revlist[-1]
 
     requests = \
-        trigger_job(rev, buildername, times=times, dry_run=dry_run)
+        trigger_job(revision, buildername, times=times, dry_run=dry_run)
     if any(req.status_code != 202 for req in requests):
         print 'WARNING: not all requests succeded'
 
     return ('https://treeherder.mozilla.org/#/jobs?%s' % urllib.urlencode({
         'repo': repo_name,
-        'revision': rev,
+        'revision': revision,
         'filter-searchStr': buildername
     }))
